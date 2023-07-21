4630a5f455a91c02a51bf2ede72ae1c3c1f3b8da
rbhushan334 <ravi.bhushan@dhwaniris.com>
2023-07-21 17:46:30 +0530

    GLOW 5.1: IST Timezone shifting

--- probabilistic/cron/sendWordleNudge.js
+++ probabilistic/cron/sendWordleNudge.js
@@ -21,8 +21,8 @@ let zcql = catalystApp.zcql();
 
 //Get the current time
 let currentDate = new Date()
-//currentDate.setHours(currentDate.getHours()+5)
-//currentDate.setMinutes(currentDate.getMinutes()+30)
+currentDate.setHours(currentDate.getHours()+5)
+currentDate.setMinutes(currentDate.getMinutes()+30)
 console.info((new Date()).toString()+"|"+prependToLog,"Current TimeStamp = ",currentDate)
 const currentHour = ("0"+currentDate.getHours()).slice(-2) + ":00"
 
