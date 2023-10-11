444f299e0ff496ec9cfe2fe6944a67cff8c05d1e
Aashutosh Chaudhary <aashutosh.chaudhary@thenudge.org>
2023-10-11 17:33:12 +0530

    fix

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const probabilistic = require("./probabilistic/index.js");
-mongoose.connect(process.env.MONGO_CONNECTION_URL);
+mongoose.connect(process.env.MONGO_CONNECTION_URL + "whatsapp-bots");
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback() {
   console.log("h");
 });
 
 const app = express();
-
-
 
 // Use the json and urlencoded middlewares
 app.use(express.json());
